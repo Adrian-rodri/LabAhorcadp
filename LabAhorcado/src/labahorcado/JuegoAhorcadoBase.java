a3c69258516749adf1331dc403800cea878818c3
Debbido27 <davidmedina272004@gmail.com>
2026-02-13 08:55:36 -0600

    Se agrego el constructor y se terminaron los metodos

--- LabAhorcado/src/labahorcado/JuegoAhorcadoBase.java
+++ LabAhorcado/src/labahorcado/JuegoAhorcadoBase.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package labahorcado;
 
-/**
- *
- * @author Dell
- */
-public class JuegoAhorcadoBase {
+package labahorcado;
+ 
+import java.util.ArrayList;
+
+public abstract class JuegoAhorcadoBase implements juegoAhorcado {
     
+    protected String palabraSecreta;
+    protected String palabraActual;
+    protected int intentos;
+    protected int limiteIntentos = 6;
+    protected ArrayList<Character> letrasUsadas;
+    protected ArrayList<String> figuraAhorcado;
+   
+    public JuegoAhorcadoBase(){
+        this.intentos=limiteIntentos;
+        this.letrasUsadas= new ArrayList();
+        this.figuraAhorcado=new ArrayList();
+        
+    }
+    
+    private void inicializarFigura(){
+        
+    }
+    protected abstract void actualizarPalabraActual(char letra);
+    protected abstract boolean verificarLetra(char letra);
+    protected abstract boolean hasGanado();
 }
