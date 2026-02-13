48cc3a8534ae98e1e51ecfe3e93481b9c0dd906a
Joshua Stan <lego24451@gmail.com>
2026-02-13 10:30:36 -0600

    avance joshua 3 juegoazar

--- LabAhorcado/src/labahorcado/JuegoAhorcadoAzar.java
+++ LabAhorcado/src/labahorcado/JuegoAhorcadoAzar.java
@@ -4,54 +4,83 @@
  */
 package labahorcado;
 
-/**
- *
- * @author spodi
- */
 import java.util.ArrayList;
 import java.util.Random;
 
+public class JuegoAhorcadoAzar extends JuegoAhorcadoBase {
 
-public abstract class JuegoAhorcadoAzar extends JuegoAhorcadoBase{
-    
-   private String[] palabras = {"java", "programa", "computadora", "teclado", "pantalla"};
-   private int intentosFallidos; 
-   
-   
-   public JuegoAhorcadoAzar(){
-       
-       intentosFallidos= 0;
-       letrasUsadas = new ArrayList<>();
-       seleccionarPalabraAleatoria();
-       
-   }
-   private void seleccionarPalabraAleatoria(){
-       Random rand = new Random(); 
-       int indice = rand.nextInt(palabras.length); 
-       palabraSecreta = palabras[indice]; 
-   }
-   
-   public void ingresarLetra(char letra){
-       if(!letrasUsadas.contains(letra)){
-           letrasUsadas.add(letra); 
-           
-           if(palabraSecreta.indexOf(letra) == -1 ){
-               intentosFallidos++; 
-           }
-       }
-   }
-   public void mostrarProgreso(){
-       for(int i = 0; i < palabraSecreta.length(); i++){
-           char c = palabraSecreta.charAt(i);
-           if(letrasUsadas.contains(c)){
-               System.out.print(c + " ");
-               
-           }else{
-               System.out.print("_ ");
-           }
-       }
-       System.out.println(); 
-   System.out.println("Intentos fallidos: " + intentosFallidos + "/" + limiteIntentos);
-   }
-   
+    private String[] palabras = {"java", "programa", "computadora", "teclado", "pantalla"};
+    private int intentosFallidos;
+
+    public JuegoAhorcadoAzar() {
+        intentosFallidos = 0;
+        letrasUsadas = new ArrayList<>();
+        inicializarPalabraSecreta();
+    }
+
+    private void seleccionarPalabraAleatoria() {
+        Random rand = new Random();
+        int indice = rand.nextInt(palabras.length);
+        palabraSecreta = palabras[indice];
+    }
+
+    @Override
+    public void inicializarPalabraSecreta() {
+        seleccionarPalabraAleatoria();
+        palabraActual = "";
+
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            palabraActual += "_";
+        }
+    }
+
+    public void ingresarLetra(char letra) {
+        if (!letrasUsadas.contains(letra)) {
+            letrasUsadas.add(letra);
+
+            if (!verificarLetra(letra)) {
+                intentosFallidos++;
+            } else {
+                actualizarPalabraActual(letra);
+            }
+        }
+    }
+
+    @Override
+    protected boolean verificarLetra(char letra) {
+        return palabraSecreta.indexOf(letra) != -1;
+    }
+
+    @Override
+    protected void actualizarPalabraActual(char letra) {
+        String nueva = "";
+
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            if (palabraSecreta.charAt(i) == letra) {
+                nueva += letra;
+            } else {
+                nueva += palabraActual.charAt(i);
+            }
+        }
+
+        palabraActual = nueva;
+    }
+
+    @Override
+    protected boolean hasGanado() {
+        return palabraActual.equals(palabraSecreta);
+    }
+
+    public void mostrarProgreso() {
+        for (int i = 0; i < palabraActual.length(); i++) {
+            System.out.print(palabraActual.charAt(i) + " ");
+        }
+        System.out.println();
+        System.out.println("Intentos fallidos: " + intentosFallidos + "/" + limiteIntentos);
+    }
+
+    @Override
+    public void jugar() {
+        
+}
 }
