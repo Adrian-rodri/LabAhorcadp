ea5ba10b09290969a5bb61561d278acb58cff3dd
Debbido27 <davidmedina272004@gmail.com>
2026-02-13 09:47:58 -0600

    Se creo la clase de administrar palabras secretas, y tambien el metodo de agregar palabras

--- LabAhorcado/src/labahorcado/AdminPalabrasSecretas.java
+++ LabAhorcado/src/labahorcado/AdminPalabrasSecretas.java
@@ -12,5 +12,19 @@ public class AdminPalabrasSecretas {
         palabras = new ArrayList<>();
     }
     
+    private void agregarPalabra(String palabra) throws IllegalArgumentException {
     
+        if(palabra == null || palabra.trim().isEmpty()){
+            throw new IllegalArgumentException("Las palabras no pueden estar vacias");
+        }
+    
+        palabra = palabra.toUpperCase();
+        
+        if(palabras.contains(palabra)){
+            throw new IllegalArgumentException ("La palabra ya existe en la lista");
+        }
+        
+        palabras.add(palabra);
+        
+    }
 }
