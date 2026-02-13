ef3d80aefcd8fa16bb8d555ca70899617c0e8229
Debbido27 <davidmedina272004@gmail.com>
2026-02-13 10:14:22 -0600

    m

--- LabAhorcado/src/labahorcado/LabAhorcado.java
+++ LabAhorcado/src/labahorcado/LabAhorcado.java
@@ -10,12 +10,11 @@ public class LabAhorcado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         AhorcadoGui juego= new AhorcadoGui();
-=======
+
         // TODO code application logic here
         
->>>>>>> e19c159d2baaf124db7a36dddb5844b886cb5d43
     }
     
 }
