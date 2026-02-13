8f056b823b6945baa9557a72eb2f7602369bd294
Joshua Stan <lego24451@gmail.com>
2026-02-13 09:47:46 -0600

    avance joshua2 juegoazar

--- LabAhorcado/src/labahorcado/JuegoAhorcadoAzar.java
+++ LabAhorcado/src/labahorcado/JuegoAhorcadoAzar.java
@@ -8,6 +8,50 @@ package labahorcado;
  *
  * @author spodi
  */
-public class JuegoAhorcadoAzar {
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public abstract class JuegoAhorcadoAzar extends JuegoAhorcadoBase{
     
+   private String[] palabras = {"java", "programa", "computadora", "teclado", "pantalla"};
+   private int intentosFallidos; 
+   
+   
+   public JuegoAhorcadoAzar(){
+       
+       intentosFallidos= 0;
+       letrasUsadas = new ArrayList<>();
+       seleccionarPalabraAleatoria();
+       
+   }
+   private void seleccionarPalabraAleatoria(){
+       Random rand = new Random(); 
+       int indice = rand.nextInt(palabras.length); 
+       palabraSecreta = palabras[indice]; 
+   }
+   
+   public void ingresarLetra(char letra){
+       if(!letrasUsadas.contains(letra)){
+           letrasUsadas.add(letra); 
+           
+           if(palabraSecreta.indexOf(letra) == -1 ){
+               intentosFallidos++; 
+           }
+       }
+   }
+   public void mostrarProgreso(){
+       for(int i = 0; i < palabraSecreta.length(); i++){
+           char c = palabraSecreta.charAt(i);
+           if(letrasUsadas.contains(c)){
+               System.out.print(c + " ");
+               
+           }else{
+               System.out.print("_ ");
+           }
+       }
+       System.out.println(); 
+   System.out.println("Intentos fallidos: " + intentosFallidos + "/" + limiteIntentos);
+   }
+   
 }
